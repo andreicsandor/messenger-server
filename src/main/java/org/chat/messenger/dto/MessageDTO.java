9ba72f9c6cccc9303492d83d996abc3de40b8f27
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-05-16 17:03:27 +0300

    Add Message & MessageDTO classes

---
+++ src/main/java/org/chat/messenger/dto/MessageDTO.java
@@ -0,0 +1,41 @@
+package org.chat.messenger.dto;
+
+public class MessageDTO {
+    private String sender;
+    private String recipient;
+    private String content;
+
+    public MessageDTO() {}
+
+    public MessageDTO(String sender, String recipient, String content) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
+
---
+++ src/main/java/org/chat/messenger/model/Message.java
@@ -0,0 +1,75 @@
+package org.chat.messenger.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String chatId;
+    private String sender;
+    private String recipient;
+    private String content;
+    private Status status;
+
+    public Message() {
+    }
+
+    public Message(String sender, String recipient, String content) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+        this.status = Status.SENT;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
